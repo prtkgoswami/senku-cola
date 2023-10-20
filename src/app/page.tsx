226efaf7d8dbc9ac038aa57c46d861ab52749c35
Pratik Goswami <prtkgoswami8@gmail.com>
2023-10-20 01:54:24 -0700

    Fix BG Color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ProductPage from "./components/ProductPage/";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-zinc-950">
       <ProductPage />
     </main>
   );
